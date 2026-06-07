a0eb446943f46e5d6b556af1e89ea1e0111924bb
KOTOEWai <149601802+KOTOEWai@users.noreply.github.com>
2026-06-07 17:49:56 +0630

    fix:setUpforPrisma

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,31 @@
 import express, { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.use(express.json());
 
-// အခြေခံ Route (TypeScript မှာ Type တွေ သတ်မှတ်ပေးထားတာ သတိပြုပါ)
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+const prisma = new PrismaClient({ adapter });
+
 app.get('/', (req: Request, res: Response) => {
-  res.json({ message: "Welcome to E-commerce TypeScript Backend API!" });
+  res.json({ message: "Hello! Prisma 7 with Express is finally running!" });
+});
+
+app.get('/users', async (req: Request, res: Response) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: "Database ချိတ်ဆက်မှု အဆင်မပြေပါ" });
+  }
 });
 
 app.listen(PORT, () => {
